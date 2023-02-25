440511dd8f1d83da774041165094c0dff57a802c
Pranav1326 <visavadiapa@gmail.com>
2023-02-25 16:28:36 +0530

    Author Profile Page

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,26 +1,40 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getAuthor } from '../../api/article';
 import ProfileArticles from '../ProfileArticles/ProfileArticles';
 
 const UserProfile = ({BASE_URL}) => {
+    
+    const { id } = useParams();
+    const [ user, setUser ] = useState(null);
+
+    useEffect(() => {
+        getAuthor(id, setUser);
+    }, []);
+    
+    if(!user) return <p className='no-posts-msg'>Loading...</p>;
     
     return (
         <div className='profile-section'>
             <section className="profile-section-1">
                 <div className="profile-pic">
-                    <img src={"https://w7.pngwing.com/pngs/831/88/png-transparent-user-profile-computer-icons-user-interface-mystique-miscellaneous-user-interface-design-smile-thumbnail.png" } alt="User Imae" className='img'/>
+                    <img src={user && user.profilepic ? user.profilepic : "https://w7.pngwing.com/pngs/831/88/png-transparent-user-profile-computer-icons-user-interface-mystique-miscellaneous-user-interface-design-smile-thumbnail.png" } alt="User Imae" className='img'/>
                 </div>
                 <div className="profile-details">
-                    <p className='user-details'>{"Pranav"}</p>
-                    <p className='user-details'>Bio: {"Developer"}</p>
-                    <p className='user-details'>Joined: {"14 Feb 2022"}</p>
-                    <p className='user-details'>Location: {"Gujarat"}</p>
-                    <p className='user-details'>Work: {"Student"}</p>
+                    <p className='user-details'>{user.username}</p>
+                    <p className='user-details'>Bio: {user.bio}</p>
+                    <p className='user-details'>Joined: {new Date(user.createdAt).toDateString()}</p>
+                    <p className='user-details'>Location: {user.location}</p>
+                    <p className='user-details'>Work: {user.work}</p>
                 </div>
             </section>
             <section className="profile-section-2">
                 <div className="details">
-                    <p>{5} Post Publised</p>
+                    <p>{user.articles.length} Post Publised</p>
+                </div>
+                <div className="details">
+                    <p>{user.comments.length} Comments</p>
                 </div>
                 {/* <ProfileArticles 
                 /> */}
